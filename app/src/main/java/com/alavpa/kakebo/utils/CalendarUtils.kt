5e82f0ff6e6ae0ed452ac5d4ea1af5f3b9b17539
alavpa <alavpa@gmail.com>
2024-12-13 10:40:28 +0100

    unit test CalendarUtils

--- app/src/main/java/com/alavpa/kakebo/utils/CalendarUtils.kt
+++ app/src/main/java/com/alavpa/kakebo/utils/CalendarUtils.kt
@@ -1,34 +1,43 @@
 package com.alavpa.kakebo.utils
 
 import android.icu.text.DateFormat
+import androidx.annotation.VisibleForTesting
 import java.util.Calendar
+import java.util.Date
 import javax.inject.Inject
 
 class CalendarUtils @Inject constructor() {
 
     fun getCurrentTimestamp(): Long {
-        return Calendar.getInstance().timeInMillis
+        return getCalendarInstance().timeInMillis
     }
 
     fun getDateFormat(timestamp: Long): String {
-        val date = Calendar.getInstance().apply {
+        val date = getCalendarInstance().apply {
             timeInMillis = timestamp
         }.time
-        return DateFormat.getPatternInstance(DateFormat.YEAR_ABBR_MONTH).format(date)
+        return format(date)
     }
 
     fun getMonth(timestamp: Long): Int {
-        return Calendar.getInstance().apply {
+        return getCalendarInstance().apply {
             timeInMillis = timestamp
         }.get(Calendar.MONTH)
     }
 
     fun getYear(timestamp: Long): Int {
-        return Calendar.getInstance().apply {
+        return getCalendarInstance().apply {
             timeInMillis = timestamp
         }.get(Calendar.YEAR)
     }
 
-    fun getCurrentMonth(): Int = Calendar.getInstance().get(Calendar.MONTH)
-    fun getCurrentYear(): Int = Calendar.getInstance().get(Calendar.YEAR)
+    fun getCurrentMonth(): Int = getCalendarInstance().get(Calendar.MONTH)
+    fun getCurrentYear(): Int = getCalendarInstance().get(Calendar.YEAR)
+
+    @VisibleForTesting
+    fun getCalendarInstance(): Calendar = Calendar.getInstance()
+
+    @VisibleForTesting
+    fun format(date: Date): String =
+        DateFormat.getPatternInstance(DateFormat.YEAR_ABBR_MONTH).format(date)
 }
---
+++ app/src/test/java/com/alavpa/kakebo/utils/CalendarUtilsTest.kt
@@ -0,0 +1,71 @@
+package com.alavpa.kakebo.utils
+
+import io.mockk.every
+import io.mockk.spyk
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.Calendar
+
+private const val TIMESTAMP: Long = 1734036363000
+
+class CalendarUtilsTest {
+
+    private val calendarUtils: CalendarUtils = spyk()
+
+    @Test
+    fun `when ask for current month return current month`() {
+        every { calendarUtils.getCalendarInstance() } returns Calendar.getInstance().apply {
+            set(Calendar.MONTH, 10)
+        }
+
+        val currentMonth = calendarUtils.getCurrentMonth()
+
+        assertEquals(10, currentMonth)
+    }
+
+    @Test
+    fun `when ask for current timestamp return current timestamp`() {
+        every { calendarUtils.getCalendarInstance() } returns Calendar.getInstance().apply {
+            timeInMillis = 10
+        }
+
+        val currentMonth = calendarUtils.getCurrentTimestamp()
+
+        assertEquals(10, currentMonth)
+    }
+
+    @Test
+    fun `when ask for current year return current year`() {
+        every { calendarUtils.getCalendarInstance() } returns Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1089)
+        }
+
+        val currentMonth = calendarUtils.getCurrentYear()
+
+        assertEquals(1089, currentMonth)
+    }
+
+    @Test
+    fun `when ask for year by timestamp return proper year`() {
+        val year = calendarUtils.getYear(TIMESTAMP)
+
+        assertEquals(2024, year)
+    }
+
+    @Test
+    fun `when ask for month by timestamp return proper month`() {
+        val month = calendarUtils.getMonth(TIMESTAMP)
+
+        assertEquals(11, month)
+    }
+
+    @Test
+    fun `when ask for current date formatted by timestamp return proper date`() {
+        val expectedDate = Calendar.getInstance().apply { timeInMillis = TIMESTAMP }.time
+        every { calendarUtils.format(expectedDate) } returns "Dec 2024"
+
+        val date = calendarUtils.getDateFormat(1734036363000)
+
+        assertEquals("Dec 2024", date)
+    }
+}
