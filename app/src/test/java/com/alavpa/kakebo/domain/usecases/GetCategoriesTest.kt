cd69fedcc0967b856a00632255b90ff5c026f394
alavpa <alavpa@gmail.com>
2024-12-13 10:45:26 +0100

    GetCategoriesTest

---
+++ app/src/test/java/com/alavpa/kakebo/domain/usecases/GetCategoriesTest.kt
@@ -0,0 +1,36 @@
+package com.alavpa.kakebo.domain.usecases
+
+import app.cash.turbine.test
+import com.alavpa.kakebo.domain.models.Category
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GetCategoriesTest {
+    private val useCase = GetCategories()
+
+    @Test
+    fun `when call use case being income should return income categories`() = runTest {
+        val incomeCategories = listOf(Category.Salary, Category.Gifts, Category.Extras)
+
+        useCase(isIncome = true).test {
+            assertEquals(incomeCategories, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `when call use case being outcome should return outcome categories`() = runTest {
+        val outcomeCategories = listOf(
+            Category.Survival,
+            Category.Leisure,
+            Category.Culture,
+            Category.Extras
+        )
+
+        useCase(isIncome = false).test {
+            assertEquals(outcomeCategories, awaitItem())
+            awaitComplete()
+        }
+    }
+}
