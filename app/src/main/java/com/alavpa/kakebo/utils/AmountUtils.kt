035ae2b7d23ad5a18a3ff8ecde4a275efdc75ae7
alavpa <alavpa@gmail.com>
2024-12-13 10:24:52 +0100

    unit test AmountUtils

--- app/src/main/java/com/alavpa/kakebo/utils/AmountUtils.kt
+++ app/src/main/java/com/alavpa/kakebo/utils/AmountUtils.kt
@@ -1,44 +1,50 @@
 package com.alavpa.kakebo.utils
 
+import androidx.annotation.VisibleForTesting
 import java.text.DecimalFormat
-import java.text.DecimalFormatSymbols
-import java.text.NumberFormat
 import java.text.ParseException
-import java.util.Locale
 import javax.inject.Inject
 
 class AmountUtils @Inject constructor() {
-
     fun parseAmountToLong(amount: String): Long {
-        val doubleValue = try {
-            DecimalFormat.getInstance(Locale.ROOT).parse(amount)?.toFloat() ?: 0f
-        } catch (parseException: ParseException) {
-            0f
-        }
-        return (doubleValue * 100f).toLong()
+        val value = parse(amount)
+        return (value * 100.0).toLong()
     }
 
     fun fromTextToCurrency(amount: String): String {
-        return amount.formatText()
+        return formatText(amount)
     }
 
     fun fromLongToCurrency(amount: Long): String {
-        return amount.toString().formatText()
+        return formatText(amount.toString())
     }
 
     fun reset(): String {
         return fromLongToCurrency(amount = 0)
     }
 
-    private fun String.formatText(): String {
-        var currentText = this
-        while (currentText.length < 4) {
-            currentText = "0$currentText"
+    private fun formatText(text: String): String {
+        val currency = (text.toLongOrNull() ?: 0) / 100.0
+        return format(currency)
+    }
+
+    @VisibleForTesting
+    fun format(currency: Double): String {
+        val df = DecimalFormat.getCurrencyInstance()
+        df.maximumFractionDigits = 2
+        df.minimumFractionDigits = 2
+        return df.format(currency)
+    }
+
+    @VisibleForTesting
+    fun parse(value: String): Double {
+        val df = DecimalFormat.getInstance()
+        df.maximumFractionDigits = 2
+        df.minimumFractionDigits = 2
+        return try {
+            df.parse(value)?.toDouble() ?: 0.0
+        } catch (exception: ParseException) {
+            0.0
         }
-        val decimalSeparator = DecimalFormatSymbols.getInstance(Locale.ROOT).decimalSeparator
-        val currency = StringBuilder(currentText).apply {
-            insert(currentText.length - 2, decimalSeparator)
-        }.toString().toDouble()
-        return NumberFormat.getCurrencyInstance().format(currency)
     }
 }
---
+++ app/src/test/java/com/alavpa/kakebo/utils/AmountUtilsTest.kt
@@ -0,0 +1,47 @@
+package com.alavpa.kakebo.utils
+
+import io.mockk.every
+import io.mockk.spyk
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AmountUtilsTest {
+
+    private val amountUtils: AmountUtils = spyk()
+
+    @Test
+    fun `when format from long should return proper currency`() {
+        every { amountUtils.format(12.0) } returns "$12.00"
+
+        val currency = amountUtils.fromLongToCurrency(1200)
+
+        assertEquals("$12.00", currency)
+    }
+
+    @Test
+    fun `when format from text should return proper currency`() {
+        every { amountUtils.format(12.0) } returns "$12.00"
+
+        val currency = amountUtils.fromTextToCurrency("1200")
+
+        assertEquals("$12.00", currency)
+    }
+
+    @Test
+    fun `when reset should return proper currency`() {
+        every { amountUtils.format(0.0) } returns "$0.00"
+
+        val currency = amountUtils.reset()
+
+        assertEquals("$0.00", currency)
+    }
+
+    @Test
+    fun `when parse from amount should return proper Long value`() {
+        every { amountUtils.parse("12.45") } returns 12.45
+
+        val value = amountUtils.parseAmountToLong("12.45")
+
+        assertEquals(1245, value)
+    }
+}
